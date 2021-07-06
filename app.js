31e14eddc8333914a5e298939762f3f133b39e31
Sultan <sultanp@gmail.com>
2021-07-07 00:01:55 +0100

    added routes

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile('./about.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile('./contact.html', { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile('./404.html', { root: __dirname });
+});
+
+app.listen(8080);
